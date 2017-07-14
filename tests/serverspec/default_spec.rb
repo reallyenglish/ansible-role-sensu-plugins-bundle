b62c2e84266579c5c8590b3745a131262245eeb3
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-07-14 14:03:45 +0900

    QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -7,18 +7,15 @@ git_package = "git"
 bundler_package = "bundler"
 user = "sensu"
 group = "sensu"
-default_user = "root"
-default_group = "root"
 
 case os[:family]
 when "freebsd"
   plugins_dir = "/usr/local/etc/sensu/plugins"
   bundled_plugins_dir = "/usr/local/etc/sensu/bundled_plugins"
   bundler_package = "rubygem-bundler"
-  default_group = "wheel"
 end
 
-[ git_package, bundler_package ].each do |p|
+[git_package, bundler_package].each do |p|
   describe package(p) do
     it { should be_installed }
   end
@@ -70,7 +67,7 @@ describe command("( cd #{bundled_plugins_dir}/sensu-plugins-load-checks && bundl
   its(:stdout) { should match(/sensu-plugins-load-checks/) }
 end
 
-describe command ("#{plugins_dir}/bundled_check sensu-plugins-load-checks check-load.rb -c 1000,1000,1000 -w 1000,1000,1000") do
+describe command("#{plugins_dir}/bundled_check sensu-plugins-load-checks check-load.rb -c 1000,1000,1000 -w 1000,1000,1000") do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should eq "" }
   its(:stdout) { should match(/\sOK:/) }
